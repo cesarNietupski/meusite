a22d1ba93d0efb0850e6cdecf858e38d2e9e7b32
cesarNietupski <44011116+cesarNietupski@users.noreply.github.com>
2026-09-08 22:06:16 -0300

    Adjust coverPosition for image alignment

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ const albums = [
     filter: "Posicionamento",
     category: "Retato de Posicionamento",
     cover: "https://drive.google.com/thumbnail?id=1_8D1eMt7w1ZcD93f8q6KzXBZQdhDzS4u&sz=w2000",
-    coverPosition: "center 35%",
+    coverPosition: "center 1%",
     alt: "Retrato de posicionamento",
     images: [
       "https://drive.google.com/thumbnail?id=1_8D1eMt7w1ZcD93f8q6KzXBZQdhDzS4u&sz=w2000",
